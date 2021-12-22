2eee1e8248610b20ee5fe73be3ffd60f05e4a3fb
HuyTo1368 <huyto1368@gmail.com>
2021-12-22 10:25:19 +0700

    10:25

--- router/showlist.js
+++ router/showlist.js
@@ -4,7 +4,17 @@ const {showlist} = require('../xuli/showlist');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const danhsach = await showlist();
+    const danhsach = await showlist(req.query.tinh,req.query.huyen, req.query.xa );
+    res.json(danhsach);
+})
+
+router.get('/showhuyen', async (req, res) => {
+    const danhsach = await showlist(req.query.tinh,req.query.huyen );
+    res.json(danhsach);
+})
+
+router.get('/showxa', async (req, res) => {
+    const danhsach = await showlist(req.query.tinh );
     res.json(danhsach);
 })
 
--- xuli/showlist.js
+++ xuli/showlist.js
@@ -2,9 +2,22 @@ const express = require('express');
 const mysql = require('mysql');
 const db = require('./index')
 
-exports.showlist = () => {
+exports.showlist = (tinh, huyen, xa) => {
     return new Promise ((resolve, reject) => {
-        db.query('SELECT fullname, CCCD,  gender, address, religion, job FROM `resume_btl` ', (err, result) => {
+        db.query(`SELECT fullname, CCCD,  gender, address, religion, job FROM view_danso WHERE province ="${tinh}" AND town = "${huyen}" AND village = "${xa}" `, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+exports.showlist = (tinh, huyen) => {
+    return new Promise ((resolve, reject) => {
+        db.query(`SELECT fullname, CCCD,  gender, address, religion, job FROM view_danso WHERE province ="${tinh}" AND town = "${huyen}" `, (err, result) => {
             if (err) {
                 reject(err);
             }
