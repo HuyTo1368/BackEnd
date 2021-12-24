2d2736d71ec252f97a122514096dd8757b28494e
HuyTo1368 <huyto1368@gmail.com>
2021-12-24 14:34:01 +0700

    huy

--- xuli/getMember.js
+++ xuli/getMember.js
@@ -1,13 +1,13 @@
 const db = require("./index")
 
-    function getMember(){
-        const query = `SELECT * FROM people WHERE id = 1`;
-        return new Promise((resolve, reject) => {
-            db.query(query, (err, results) => {
-                if (err) return reject(err);
-                return resolve(results);
-            })
+function getMember() {
+    const query = `SELECT * FROM people WHERE id = 1`;
+    return new Promise((resolve, reject) => {
+        db.query(query, (err, results) => {
+            if (err) return reject(err);
+            return resolve(results);
         })
-    }
+    })
+}
 
 module.exports = getMember;
