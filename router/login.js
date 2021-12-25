7cd1544d3685c566c95f4601da9aa4220bb9d149
HuyTo1368 <huyto1368@gmail.com>
2021-12-25 08:51:40 +0700

    1111

--- router/login.js
+++ router/login.js
@@ -4,20 +4,19 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const danhsach = await checkLogin(req.body);
-  if (danhsach.length) {
-    jwt.sign({ user: danhsach[0] }, "secretkey", (err, token) => {
-      danhsach[0].token = token
-
-      res.json(danhsach[0]);
-      // res.json({
-      //   token,
-      //   user,
-      // });
-    });
-  } else {
-    res.sendStatus(403);
-  }
+    const danhsach = await checkLogin(req.body);
+    if (danhsach.length) {
+        jwt.sign({ user: danhsach[0] }, "secretkey", (err, token) => {
+            danhsach[0].token = token
+            res.json(danhsach[0]);
+            // res.json({
+            //   token,
+            //   user,
+            // });
+        });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
