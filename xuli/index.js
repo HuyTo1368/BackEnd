65f8f2869bf92210161d1baed2af7c3670903cf0
Đỗ Quang Huynh <doquanghuynh2911@gmail.com>
2021-12-26 16:56:20 +0700

    Huynh 26/12 4h55

--- xuli/index.js
+++ xuli/index.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'population2'
+    database: 'population'
 })
 db.connect((err) => {
     if (err) throw err;
